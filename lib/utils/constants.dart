7221aaa74d6ec9c605d720500b5f63e166ea8c00
Hyojeong Jun <junnie082@gmail.com>
2024-01-25 21:07:35 +0900

    feat: 다이어리 기본 틀 UI

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-Color kpurpleDarkerColor = Color(0xFF7500BC);
-Color kpurpleColor = Color(0xFFD1A5EC);
-Color kpurpleLightColor = Color(0xFFF1D8FF);
+Color kpurpleDarkerColor = const Color(0xFF7500BC);
+Color kpurpleColor = const Color(0xFFD1A5EC);
+Color kpurpleLightColor = const Color(0xFFF1D8FF);
